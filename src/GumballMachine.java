3d676bbce6038a223390f65396af35cc74e1f10e
Purple2018 <53633281+Purple2018@users.noreply.github.com>
2026-02-05 22:09:29 -0800

    Rough draft of GumballMachine.java

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,38 +1,92 @@
+import java.util.ArrayList;
+
 public class GumballMachine {
 
+    //Gumball machine's balance
     private int balance;
-    private int rejected;
 
+    //Invalid input to machine, which will be returned when dispenser lever is pressed
+    private ArrayList<String> rejected;
+
+    //prices of gumballs
     private final int RED_PRICE = 5;
     private final int YELLOW_PRICE = 10;
 
-    private boolean isValidCoin(int coin) {
-
-        return coin == 5 || coin == 10 || coin == 25;
+    public GumballMachine(){
+        rejected = new ArrayList<String>();
+        balance = 0;
     }
 
+    //Validates user input as valid currency to add to balance OR invalid input to be returned later
+    private boolean isValidCoin(String input) {
+        try{
+            int coin = Integer.parseInt(input);
+            if(coin == 5 || coin == 10 || coin == 25){
+                insertCoin(coin);
+                return true;
+            }
+            else{
+                rejected.add(input);
+                return false;
+            }
+        }
+        catch(NumberFormatException e){
+            rejected.add(input);
+            return false;
+        }
+    }
+
+    //Add valid currency to machine balance
     private void insertCoin(int coin){
+        balance += coin;
+        showBalance();
 
     }
 
+    //Returns invalid user input
     private void returnRejectedCoins(){
-
+        for(String item : rejected){
+            System.out.println(item);
+        }
+        rejected = new ArrayList<String>();
     }
 
+    //Dispenses 1 red gumball if machine balance is sufficient
     public void dispensedRed(){
-
+        if(balance >= RED_PRICE){
+            balance -= RED_PRICE;
+            System.out.println("1 red gumball dispensed.");
+            showBalance();
+        }
+        else{
+            System.out.println("Error. Insufficient funds. Gumball not able to be dispensed.");
+            showBalance();
+        }
     }
 
+    //Dispenses 1 yellow gumball if machine balance is sufficient
     public void dispensedYellow(){
-
+        if(balance >= YELLOW_PRICE){
+            balance -= YELLOW_PRICE;
+            System.out.println("1 yellow gumball dispensed.");
+            showBalance();
+        }
+        else{
+            System.out.println("Error. Insufficient funds. Gumball not able to be dispensed.");
+            showBalance();
+        }
     }
 
+    //Returns remaining machine balance
     public void returnMyChange(){
-
+        System.out.println(balance + " cents have been returned.");
+        balance = 0;
+        showBalance();
     }
 
+    //Prints out current machine balance
     public void showBalance(){
-
+        System.out.println("Balance = " + balance);
     }
 
 }
